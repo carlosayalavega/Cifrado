6ec87f2eb590c41cd87d66e736468b6194e65113
carlosAyalaVega <carlos_memito@hotmail.com>
2018-05-30 22:50:36 -0500

    operaciones, validaciones ingreso, borrado, final

--- PracticaCifradoSimetrico.java
+++ PracticaCifradoSimetrico.java
@@ -3,57 +3,60 @@ package practicacifradosimetrico;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.util.*;
 
 public class PracticaCifradoSimetrico {
                 static int[] miCifrado;
                 static int n = 7;
                 static String miLlave;
+                static boolean validacion = true;
+                
 	public static void main(String[] args) {
 
 		JFrame.setDefaultLookAndFeelDecorated(true);
 		JFrame frame = new JFrame(" Algoritmo de Cifrado Simetrico ");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-    JTextArea Encriptado = new JTextArea();
-    JTextArea desEncriptado = new JTextArea();
-    JTextArea operaciones = new JTextArea();
+                JTextArea Encriptado = new JTextArea();
+                JTextArea desEncriptado = new JTextArea();
+                JTextArea operaciones = new JTextArea();
+
+                JButton encriptarB = new JButton(" Iniciar Cifrado ");
+                JButton desencriptarB = new JButton(" Iniciar Descifrado ");
+                JButton BorrarB = new JButton(" Borrar resultados ");
+
+                JTextField claveLlave1 = new JTextField(20);
+                JTextField aEncriptar = new JTextField(20);
+                JTextField aDesencriptar = new JTextField(20);
+
+                JLabel j1 = new JLabel(" Texto a cifrar ");
+                JLabel j2 = new JLabel(" Clave / LLave ");
+                //JLabel j3 = new JLabel(" Texto a descifrar ");
+
+                JScrollPane scrollPanel1 = new JScrollPane(Encriptado, ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+                scrollPanel1.setPreferredSize(new Dimension(300, 200));
+
+                JScrollPane scrollPanel2 = new JScrollPane(desEncriptado, ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+                scrollPanel2.setPreferredSize(new Dimension(300, 200));
+                
+                JScrollPane scrollPanel3 = new JScrollPane(operaciones, ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+                scrollPanel3.setPreferredSize(new Dimension(400, 200));
+                
+                JScrollPane scrollPanel4 = new JScrollPane(aDesencriptar, ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+                scrollPanel4.setPreferredSize(new Dimension(300, 100));
+                
+                JPanel panel1 = new JPanel();
+                JPanel panel2 = new JPanel();
+                JPanel panel3 = new JPanel();
+                JPanel panel4 = new JPanel();
+                JPanel panel5 = new JPanel();
+
+                panel1.setBorder(BorderFactory.createTitledBorder(""));
+                panel2.setBorder(BorderFactory.createTitledBorder("Texto Cifrado"));
+                panel3.setBorder(BorderFactory.createTitledBorder("Texto a descifrar"));
+                panel4.setBorder(BorderFactory.createTitledBorder("Texto Descrifrado"));
+                panel5.setBorder(BorderFactory.createTitledBorder("Operaciones"));
     
-    JButton encriptarB = new JButton(" Iniciar Cifrado ");
-    JButton desencriptarB = new JButton(" Iniciar Descifrado ");
-    JButton BorrarB = new JButton(" Borrar resultados ");
-
-    JTextField claveLlave1 = new JTextField(20);
-    JTextField aEncriptar = new JTextField(20);
-    JTextField aDesencriptar = new JTextField(20);
-    
-    JLabel j1 = new JLabel(" Texto a cifrar ");
-    JLabel j2 = new JLabel(" Clave / LLave ");
-    JLabel j3 = new JLabel(" Texto a descifrar ");
-
-
-    JScrollPane scrollPanel1 = new JScrollPane(Encriptado, ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-    scrollPanel1.setPreferredSize(new Dimension(300, 200));
-
-    JScrollPane scrollPanel2 = new JScrollPane(desEncriptado, ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-    scrollPanel2.setPreferredSize(new Dimension(300, 200));
-
-    JScrollPane scrollPanel3 = new JScrollPane(operaciones, ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-    scrollPanel3.setPreferredSize(new Dimension(300, 200));
-
-    JPanel panel1 = new JPanel();
-    JPanel panel2 = new JPanel();
-    JPanel panel3 = new JPanel();
-    JPanel panel4 = new JPanel();
-    JPanel panel5 = new JPanel();
-
-    panel1.setBorder(BorderFactory.createTitledBorder(""));
-    panel2.setBorder(BorderFactory.createTitledBorder("Texto Cifrado"));
-    panel3.setBorder(BorderFactory.createTitledBorder(""));
-    panel4.setBorder(BorderFactory.createTitledBorder("Texto Descrifrado"));
-    panel5.setBorder(BorderFactory.createTitledBorder("Operaciones"));
-    
-
-
 		BoxLayout layout1 = new BoxLayout(panel1, BoxLayout.Y_AXIS);
 		BoxLayout layout2 = new BoxLayout(panel2, BoxLayout.Y_AXIS);
                 BoxLayout layout3 = new BoxLayout(panel3, BoxLayout.Y_AXIS);
@@ -74,41 +77,34 @@ public class PracticaCifradoSimetrico {
 
                 panel2.add(scrollPanel1);
 
-                panel3.add(j3);
-                panel3.add(aDesencriptar);
+                panel3.add(scrollPanel4);
                 panel3.add(desencriptarB);
 
                 panel4.add(scrollPanel2);
                 
                 panel5.add(scrollPanel3);         
                 
-
 		frame.setLayout(new FlowLayout());
 		frame.add(panel1);
                 frame.add(panel2);
                 frame.add(panel3);
                 frame.add(panel4);
                 frame.add(panel5);
-                frame.add( BorrarB);
+                frame.add(BorrarB);
 
 		frame.pack();
-		frame.setVisible(true);
+                frame.setVisible(true);
                 frame.setSize(1400,500);
-                //frame.setBackground(Color.blue);
                 frame.setLocationRelativeTo(null);
-
+                
                BorrarB.addActionListener((ActionEvent e) -> {
                    aEncriptar.setText(null);
                    claveLlave1.setText(null);
                    Encriptado.setText(null);
                    desEncriptado.setText(null);
                    aDesencriptar.setText(null);
-
-                   //aEncriptar.setText("");
-                   //claveLlave1.setText("");
-                   //Encriptado.setText("");
-                   //desEncriptado.setText("");
-                   //aDesencriptar.setText("");
+                   operaciones.setText(null);
+                
                 });
 
                 encriptarB.addActionListener((java.awt.event.ActionEvent evt) -> {
@@ -120,11 +116,28 @@ public class PracticaCifradoSimetrico {
                         String transtFinal="";
                         String finalCif = "";
                         int elMod;
-
+                        
+                        
                         miSust = aEncriptar.getText();
                         miLlave = claveLlave1.getText();
-
+                        
+                        for (int i = 0; i < miSust.length(); i++){
+                            if (Character.isDigit(miSust.charAt(i))== true){
+                                    validacion = false;
+                                    aEncriptar.setText("");
+                                    claveLlave1.setText("");
+                                                                     
+                            JOptionPane.showMessageDialog(null,"Debes ingresar solo letras a cifrar ", " Error! ", JOptionPane.ERROR_MESSAGE);
+                            break;
+                            }
+                        }
+                        
+                                           
+                                                
                         for(int i=0 ; i<miSust.length() ; i++) {
+                            if(validacion = false){
+                                break;
+                            }
                             char miVar = miSust.charAt(i);
                             sustFinal+= ((char)(miVar-6));
                         }
@@ -148,29 +161,32 @@ public class PracticaCifradoSimetrico {
                                 
                                 transtFinal+=(miVar);
                             }
+                            operaciones.append("\n ");  
                         }
-                        operaciones.append("\n ");              
+                                    
                         operaciones.append("\nFinal cifrado: :");
                         operaciones.append("\n"+transtFinal);
                         operaciones.append("\n ");
 
                         operaciones.append("\nCesar cifrado");
+                        if(validacion = true){
                         oux = cifrar(transtFinal, miLlave);
                         operaciones.append("\n"+oux);
                         operaciones.append("\n ");
-
+                        
                         //XOR cifrado
                         operaciones.append("\nXOR cifrado");
                         operaciones.append("\n ");
                         miCifrado = encrXOR(oux,miLlave);
                         for(int i = 0; i < miCifrado.length; i++){
                             operaciones.append(Integer.toString(miCifrado[i])+",");
-                            finalCif+=(miCifrado[i]+",");
+                            finalCif+=("\n"+miCifrado[i]+",");
                         }
                         
                         Encriptado.setText(finalCif);
                         aDesencriptar.setText(finalCif);
-
+                        }
+                        
                     }catch(Exception e){
                         JOptionPane.showMessageDialog(null,"Texto a cifrar incorrecto", " Error! ", JOptionPane.ERROR_MESSAGE);
                     }
@@ -180,12 +196,12 @@ public class PracticaCifradoSimetrico {
                             try{
                                 String conTrans ="";
                                 String normal="";
-                                //XOR descifrado
+                                
                                 operaciones.append("\n ");
                                 operaciones.append("\nXOR descifrado");
                                 String xorDescifrado = desencrXOR(miCifrado,miLlave);
                                 operaciones.append("\n"+xorDescifrado);
-                                //Cesar descifrado
+                                
                                 operaciones.append("\n ");
                                 operaciones.append("\nCesar descifrado");
                                 String cesarDescifrado = descifrar(xorDescifrado, miLlave);
@@ -207,7 +223,8 @@ public class PracticaCifradoSimetrico {
                                 operaciones.append("\n ");
                                 operaciones.append("\nTexto = resultado descifrado: ");
                                 operaciones.append("\n"+normal);
-
+                                char[] charArray = normal.toCharArray();
+                                char arr[]=normal.toCharArray();
                                 desEncriptado.setText(normal);
 
                             }catch(Exception e){
@@ -252,3 +269,4 @@ public class PracticaCifradoSimetrico {
                 return Mifinal;
             }
  }
+
